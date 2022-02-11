c3e8fa0ccf905ab7b058f0385678824f22fd3d81
TatianaHorbenko <3435640@gmail.com>
2022-02-11 21:30:09 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,12 +1,17 @@
-const spanColorEl = document.querySelector('span.color');
-const changeColorBtn = document.querySelector('button.change-color');
-const bodyColorEl = document.querySelector('body');
+
+const spanColorEl = document.querySelector(".color");
+const changeColorBtn = document.querySelector(".change-color");
+const bodyColorEl = document.querySelector("body");
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-function onChangeColor(){
-  bodyColorEl.style.backgroundColor = getRandomHexColor();
-  spanColorEl.textContent = bodyColorEl.style.background
+const onChangeColor=()=>{
+  const chandeColor = getRandomHexColor();
+
+  bodyColorEl.setAttribute("style", `background:${chandeColor} `);
+  spanColorEl.textContent = chandeColor;
 }
 changeColorBtn.addEventListener("click", onChangeColor);
+
+
